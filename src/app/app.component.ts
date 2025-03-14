44ea57893ccafd212ed66055f5fa022e9f4f0769
YznMohsen <y.mohsen@smartsoft-co.com>
2025-03-14 14:48:21 +0300

    • Prepare Main Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  ,
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
   title = 'restaurant-manager';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Modules/MainModule/header/header.component';
+import { FooterComponent } from './Modules/MainModule/footer/footer.component';
+import { HomeComponent } from './Modules/MainModule/home/home.component';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgOptimizedImage
   ],
   providers: [],
   bootstrap: [AppComponent]
